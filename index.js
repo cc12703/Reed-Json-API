3e2e17a826f3eabbc570080ef296eaf999bcf7fa
HansHuang <hys0550@sina.com>
2019-10-18 08:47:56 +0800

    update for test

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const fs = require('fs'),
 
 //TODO: watch file changed for json template
 
-module.exports = (options) => {
-    const { urlPrefix, filePath } = options,
-        template = fs.readFileSync(filePath).toString(),
+module.exports = ({ urlPrefix, filePath }) => {
+    const template = fs.readFileSync(filePath).toString(),
         jsonResult = dummyJson.parse(template),
         jsonData = JSON.parse(jsonResult),
         router = new Router({ prefix: urlPrefix || '' })
 
-    router.get('/:name/:id?', require('./get')(jsonData))
-        .delete('/:name/:id?', require('./delete')(jsonData))
+    router.get('/:name/:id?', require('./lib/get')(jsonData))
+        .delete('/:name/:id?', require('./lib/delete')(jsonData))
         .all('/:name?', handleBadRequest)
-        .post('/:name?', require('./create')(jsonData))
-        .put('/:name/:id?', require('./update')(jsonData, false))
-        .patch('/:name/:id?', require('./update')(jsonData, true))
+        .post('/:name?', require('./lib/create')(jsonData))
+        .put('/:name/:id?', require('./lib/update')(jsonData, false))
+        .patch('/:name/:id?', require('./lib/update')(jsonData, true))
 
     return router.routes()
 }
--- test/app.js
+++ test/app.js
@@ -1,3 +1,14 @@
-
+const Koa = require('koa'),
+    path = require('path'),
+    api = require('../index')
 
 process.env.NODE_ENV = 'test'
+
+exports.startServer = function (port) {
+    const app = new Koa(),
+        filePath = path.join(process.cwd(), 'test', 'json-api.hbs')
+
+    app.use(api({ urlPrefix: 'api', filePath }))
+
+    return app.listen(port)
+}
